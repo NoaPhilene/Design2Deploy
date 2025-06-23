77cfc75dc143e8d02ff533e083517d0f2e370b39
liannefm <lfmeiresonne@gmail.com>
2025-06-23 14:29:05 +0200

    contactform

--- contact.php
+++ contact.php
@@ -3,9 +3,18 @@ include("includes/header.php");
 ?>
 
 <body>
-<div id="background">
+<div id="backgroundcontact">
     <div id="contactbox">Contact</div>
-    <div id="introtekst">Ik streef ernaar om constant in contact te staan met onze klanten totdat de klus geklaard is. Als u vragen of speciale verzoeken heeft, stuur ons dan een bericht. Voor een vrijblijvende offerte kunt u contact met ons opnemen wanneer het u uitkomt. Wij zijn u graag van dienst!</div>
+    <div id="introtekstcontact">Ik streef ernaar om constant in contact te staan met onze klanten totdat de klus geklaard is. Als u vragen of speciale verzoeken heeft, stuur ons dan een bericht. Voor een vrijblijvende offerte kunt u contact met ons opnemen wanneer het u uitkomt. Wij zijn u graag van dienst!</div>
+    
+    <div id="box1">
+        <div id="contactform">Contactformulier<br><br>
+            E-mail: <input type="email" required><br>
+            Telefoonnummer: <input type="number" required><br>
+            Opmerking: <textarea id="informatie" name="informatie" cols= "26" rows="4"></textarea>
+        </div>
+        <img src="assets/fotos/hendrik1.webp" alt="hendrik1" id="hendrikrond">
+    </div>
     
 </div>
 
