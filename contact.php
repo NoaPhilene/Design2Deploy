9bc753f5decf39051ebc4d628796a1a9d9e2c96b
liannefm <lfmeiresonne@gmail.com>
2025-06-24 16:08:58 +0200

    teksten contactpagina

--- contact.php
+++ contact.php
@@ -8,12 +8,54 @@ include("includes/header.php");
     <div id="introtekstcontact">Ik streef ernaar om constant in contact te staan met onze klanten totdat de klus geklaard is. Als u vragen of speciale verzoeken heeft, stuur ons dan een bericht. Voor een vrijblijvende offerte kunt u contact met ons opnemen wanneer het u uitkomt. Wij zijn u graag van dienst!</div>
     
     <div id="box1">
-        <div id="contactform">Contactformulier<br><br>
-            E-mail: <input type="email" required><br>
-            Telefoonnummer: <input type="number" required><br>
-            Opmerking: <textarea id="informatie" name="informatie" cols= "26" rows="4"></textarea>
+        <div id="contactformulier">
+        <h2>Contactformulier</h2>
+            <div id="formulierinhoud">
+            <div id="formnamen">
+                <div>E-mail:</div>
+                <div>Telefoonnummer:</div>
+                <div>Opmerking:</div>
+            </div>
+            <div id="formvelden">
+                <input type="email" required>
+                <input type="tel" required>
+                <textarea cols="26" rows="4"></textarea>
+
+                <input type="submit" value="Verstuur">
+            </div>
+            </div>
         </div>
         <img src="assets/fotos/hendrik1.webp" alt="hendrik1" id="hendrikrond">
+    </div>
+
+    <div id="box2">
+        <img src="assets/fotos/hendrik3.webp" alt="hendrik3" id="hendrikvierkant">
+
+        <div id="gegevenshendrik">
+            <h2>Mijn contactgegevens</h2>
+            <div id="gegevens">
+                Telefoonnummer: 06 98765432<br>
+                E-mail: hendrikhogendijkhovenier@gmail.com
+            </div>
+        </div>
+    </div>
+
+    <div id="box3">
+        <div id="werkdagen">
+            <h2>Werkdagen</h2>
+            <div id="werktijden">
+                Maandag: 7:00-16:00
+                Dinsdag: 7:00-16:00
+                Woensdag: 7:00-16:00
+                Donderdag: 7:00-16:00
+                Vrijdag: 7:00-16:00
+                Zaterdag: Op afspraak
+                Zondag: Gesloten
+            </div>
+        </div>
+        <div id="maps">
+
+        </div>
     </div>
     
 </div>
