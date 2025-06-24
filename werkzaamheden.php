e2aaaf31db42ccf93923202b3844ec9bf81dc8e7
Darindyb <darin.detoekomst@gmail.com>
2025-06-24 16:10:52 +0200

    vierkantjes

--- werkzaamheden.php
+++ werkzaamheden.php
@@ -4,6 +4,9 @@ include("includes/header.php");
 
 <body>
     <div id="backgroundwerkzaamheden">
-        
+        <div id="tuinontwerp"></div>
+        <div id="tuinaanleg"></div>
+        <div id="tuinonderhoud"></div>
+        <div id="zwemvijvers"></div>
     </div>
 </body>
