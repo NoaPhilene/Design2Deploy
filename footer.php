0b9a0e28d3cab7ae257b134d6d539c0ce4c4f51a
SheneB07 <shenebanga@gmail.com>
2025-06-25 09:05:36 +0200

    footer

---
+++ footer.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<footer>
+        <div class="footer-content">
+             <div class="row">
+            <div class="column">
+                <h2>Werkdagen: </h2>
+            </div>    
+            <div class="column">
+                
+            </div>
+                <div class="column">
+                 <h2>Contact gegevens:</h2>
+                 
+             </div>
+             <div class="column">
+                 <h2>Werkzaamheden:</h2>
+                 
+             </div>
+            </div>
+
+            <div class="row">
+                <div class="column">
+                <h3>Maandag: 7:00 - 16:00<br>
+                Dinsdag: 7:00 - 16:00 <br>
+                Woensdag: 7:00 - 16:00 <br>
+                Donderdag: 7:00 - 16:00 <br>
+                Vrijdag: 7:00 - 16:00 <br>
+                Zaterdag: Op afspraak<br>
+                Zondag: Gesloten </h3><br>
+                <h3>
+            </div>
+            <div class="column">
+                <h4>
+                    Telefoonnummer: 06 9876543<br>
+                    Email: hendrikhogendijkhovenier@gmail.com<br>
+                 </h4>
+            </div>
+            <div class="column">
+                <h3>
+                 <ul>
+                    <li>Tuinontwerp</li>
+                    <li>Tuinaanleg</li>
+                    <li>Tuinonderhoud</li>
+                    <li>Zwemvijvers</li>
+                </ul>
+                </h3>
+            </div>
+            </div>
+
+            <div class="footer-end">
+            <div class="column">
+            <p>&copy; Hendrik Hogendijk</p>
+            </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+
+</body>
+</html>
