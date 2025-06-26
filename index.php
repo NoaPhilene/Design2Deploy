a377afb84884812caa9a416cb663efb08ac16b12
NoaPhilene <philenevanmil@gmail.com>
2025-06-26 14:35:29 +0200

    slideshow 1 af + alles goed staat

--- index.php
+++ index.php
@@ -13,49 +13,49 @@ include("includes/header.php");
 
             <div class="slideshow-container">
 
-            <div class="mySlides fade">
-                <img src="assets/fotos/tuinFoto1.webp" >
+                <div class="mySlides fade">
+                    <img src="assets/fotos/tuinFoto1.webp" >
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="assets/fotos/tuinFoto7.webp" >
+                </div>
+
+                <div class="mySlides fade">
+                        <img src="assets/fotos/hendrik3.webp" >
+                </div>
+
+                <div class="mySlides fade">
+                        <img src="assets/fotos/tuinFoto5.webp" >
+                </div>
+
+                <div class="mySlides fade">
+                        <img src="assets/fotos/hendrik2.webp" >
+                </div>
+
+                <div class="mySlides fade">
+                        <img src="assets/fotos/tuinFoto9.webp" >
+                </div>
+
+            </div>
+            <br>
+            <div id= SlideshowRondjes>
+                <a class="prev" onclick="plusSlides(-1)">❮</a>
+                <span class="dot" onclick="currentSlide(1)"></span> 
+                <span class="dot" onclick="currentSlide(2)"></span> 
+                <span class="dot" onclick="currentSlide(3)"></span> 
+                <span class="dot" onclick="currentSlide(4)"></span> 
+                <span class="dot" onclick="currentSlide(5)"></span>
+                <span class="dot" onclick="currentSlide(6)"></span>
+                <a class="next" onclick="plusSlides(1)">❯</a>
             </div>
 
-            <div class="mySlides fade">
-                <img src="assets/fotos/tuinFoto7.webp" >
+            
+            <div id="beginTextVakWelkom">
+                <p>Welkom bij de website van Hendrik Hogendijk, hovenier in de regio Utrecht, Zeist en de Bilt.</p>
             </div>
-
-            <div class="mySlides fade">
-                    <img src="assets/fotos/hendrik3.webp" >
-            </div>
-
-            <div class="mySlides fade">
-                    <img src="assets/fotos/tuinFoto5.webp" >
-            </div>
-
-            <div class="mySlides fade">
-                    <img src="assets/fotos/hendrik2.webp" >
-            </div>
-
-             <div class="mySlides fade">
-                    <img src="assets/fotos/tuinFoto9.webp" >
-            </div>
-
-        </div>
-        <br>
-        <div style="text-align:center">
-            <span class="dot"></span> 
-            <span class="dot"></span> 
-            <span class="dot"></span> 
-            <span class="dot"></span> 
-            <span class="dot"></span>
-            <span class="dot"></span>
-        </div>
-
-        <a class="prev" onclick="plusSlides(-1)">❮</a>
-        <a class="next" onclick="plusSlides(1)">❯</a>
-        
-        <div id="beginTextVakWelkom">
-            <p>Welkom bij de website van Hendrik Hogendijk, hovenier in de regio Utrecht, Zeist en de Bilt.</p>
         </div>
        
-        </div>
 
         <div id="werkzaamheden">Werkzaamheden
             <div class="knoppenwerkzaamheden">
