895d4e5d4b1a2940cadfac6e4948fa5e10f14ba7
Darindyb <darin.detoekomst@gmail.com>
2025-06-30 21:57:26 +0200

    blokjes

--- werkzaamheden.php
+++ werkzaamheden.php
@@ -4,9 +4,10 @@ include("includes/header.php");
 
 <body>
     <div id="backgroundwerkzaamheden">
-        <div id="tuinontwerp"></div>
-        <div id="tuinaanleg"></div>
-        <div id="tuinonderhoud"></div>
-        <div id="zwemvijvers"></div>
+        <div id="tuinontwerp">Tuinontwerp</div>
+        <div id="tuinaanleg">Tuinaanleg</div>
+        <div id="tuinonderhoud">Tuinonderhoud</div>
+        <div id="zwemvijvers">Zwemvijvers</div>
+        <div id="afgelopen">Afgelopen Services</div>
     </div>
 </body>
