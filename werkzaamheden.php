e52d7378be48bb531dff041d1216c612c78bb91d
Darindyb <darin.detoekomst@gmail.com>
2025-07-01 14:16:23 +0200

    aanpassingen werkzaamheden

--- werkzaamheden.php
+++ werkzaamheden.php
@@ -1,14 +1,19 @@
 <?php
 include("includes/header.php");
-include("includes/topbar.php");
 ?>
 
 <body>
   <div id="backgroundwerkzaamheden">
-    <div class="blok">Tuinontwerp</div>
-    <div class="blok">Tuinaanleg</div>
-    <div class="blok">Tuinonderhoud</div>
-    <div class="blok">Zwemvijvers</div>
-    <div id="afgelopen">Afgelopen Services</div>
+    <div id="blok">
+      <div class="blok">Tuinontwerp</div>
+      <div class="blok">Tuinaanleg</div>
+    </div>
+    <div id="blok1">
+      <div class="blok1">Tuinonderhoud</div>
+      <div class="blok1">Zwemvijvers</div>
+    </div>
+    <div class="afgelopen">
+      <div id="afgelopen">Afgelopen Services</div>
+    </div>
   </div>
 </body>
