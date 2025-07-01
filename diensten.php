7a5cfc42761b1d6c55fc4da9367236f836fb62c6
liannefm <lfmeiresonne@gmail.com>
2025-07-01 14:25:29 +0200

    dienstenpagina fixen

--- diensten.php
+++ diensten.php
@@ -3,6 +3,22 @@ include("includes/header.php");
 include("includes/topbar.php");
 ?>
 
+<body>
+  <div id="backgroundwerkzaamheden">
+    <div id="blok">
+      <div class="blok">Tuinontwerp</div>
+      <div class="blok">Tuinaanleg</div>
+    </div>
+    <div id="blok1">
+      <div class="blok1">Tuinonderhoud</div>
+      <div class="blok1">Zwemvijvers</div>
+    </div>
+    <div class="afgelopen">
+      <div id="afgelopen">Afgelopen Services</div>
+    </div>
+  </div>
+</body>
+
 <?php
 include("includes/footer.php");
 ?>
