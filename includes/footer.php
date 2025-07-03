1155986c6f2d3bb51d4511ef56eda82f3aa2877c
SheneB07 <shenebanga@gmail.com>
2025-07-03 23:55:16 +0200

    Nog meer footer aanpassingen

--- includes/footer.php
+++ includes/footer.php
@@ -1,24 +1,12 @@
 <footer>
     <div class="footer-content">
-            <div class="row">
-        <div class="column">
-            <div id="footer-titels">Werkdagen: </div>
-        </div>
-            <div class="column">
-                <a href="contact.php">
-                <div id="footer-titels">Contact gegevens:</div>
-            </a>
-                
-            </div>
-            <div class="column">
-                <div id="footer-titels">Werkzaamheden:</div>
-                
-            </div>
-        </div>
 
-        <div class="row">
-            <div class="column">
-            <div id="footer-linksrechts">Maandag: 7:00 - 16:00<br>
+            <div class="row">
+
+        <div class="column">
+             <div class="footer-section">
+            <div id="footer-titels">Werkdagen: </div>
+             <div id="footer-linksrechts">Maandag: 7:00 - 16:00<br>
             Dinsdag: 7:00 - 16:00 <br>
             Woensdag: 7:00 - 16:00 <br>
             Donderdag: 7:00 - 16:00 <br>
@@ -27,28 +15,43 @@
             Zondag: Gesloten <br>
             </div>
         </div>
-        <div class="column">
-            <div id="footer-middle">
+        </div>
+
+            <div class="column">
+                <div class="footer-section">
+                <a href="contact.php">
+                <div id="footer-titels">Contact gegevens:</div>
+                 <div id="footer-middle">
                 Telefoonnummer: 06 9876543<br>
                 Email: hendrikhogendijkhovenier@gmail.com<br>
             </div>
-        </div>
-        <div class="column">
-            <div id="footer-linksrechts">
+            </div>
+            </a>
+                
+            </div>
+            <div class="column">
+                <div class="footer-section">
+                <div id="footer-titels">Werkzaamheden:</div>
+                <div id="footer-linksrechts">
                 <ul>
                 <li>Tuinontwerp</li>
                 <li>Tuinaanleg</li>
                 <li>Tuinonderhoud</li>
                 <li>Zwemvijvers</li>
             </ul>
-          </div>
-        </div>
-        </div>
+</div>
 
+          </div>
+                
+            </div>
+
+        </div>
+        <br>
         <div class="footer-end">
-        <div class="column">
-        <p>&copy; Hendrik Hogendijk</p>
-        </div>
-        </div>
-    </div>
+            <div class="column">
+            <p>&copy; Hendrik Hogendijk</p>
+            </div>
+            </div>
+        
+
 </footer>
